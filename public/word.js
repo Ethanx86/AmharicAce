14d17e44777ba835b84399cf837e2d25b49a0b91
Ethanx86 <79873628+Ethanx86@users.noreply.github.com>
2023-11-06 09:59:18 -0500

    11-6-23 9:58 AM
    This is insanity. The custom-word element isn't working. Might look into whether or not word.js was imported into test.htm.

--- public/word.js
+++ public/word.js
@@ -6,8 +6,11 @@ class Word {
     connectedCallback(){
         const before = this.innerHTML.split('<meaning>')[0];
         const after = this.innerHTML.split('<meaning>')[1];
-        alert(before);
-        alert(after);
+        document.write(before);
+        this.innerHTML = `<details>
+            <summary>${before}</summary>
+            ${after}
+        </details>`;
     }
 }
 

5a238458041831f0639e93b1e04de528343f4b7b
eyob handero <eyobpetros@yahoo.com>
2023-11-07 10:14:57 -0500

    11-7-23 10:12 AM
    Slowly descending into madness. Responsive.js isn't able to make the page-title elements scale accordingly. No internet. Must wait until help/ChatGPT arrives. My sanity can't hold out for much longer. If you are reading this and these changes are published to GitHub, that means internet finally came.

--- public/elements.js
+++ public/elements.js
@@ -6,18 +6,19 @@ class PageTitle extends HTMLElement{
       const before = this.innerHTML.split('<split>')[0];
       const after = this.innerHTML.split('<split>')[1];
         this.innerHTML = `
-          <div style='border-style:none none solid none;border-width:2px;top:0px;position:fixed;width:100%;margin-left:5px;margin-top:2px;text-align:center;overflow-y:visible;'>
+          <div style='top:0px;position:fixed;width:100%;margin-left:5px;margin-top:2px;text-align:center;overflow-y:visible;'>
             <image src="./Logo.png"></image>
-            <div style='top:0px;position:fixed;width:${10}px;margin-right:5px;margin-top:2px;height:28px;text-align:right;font-size:28px;'>
+            <div style='top:0px;position:fixed;width:10px;margin-right:5px;margin-top:2px;height:28px;text-align:right;font-size:28px;'>
             ${before}
           </div>
             <b style="color:#b7111b; font-family:'Courier';font-size:28px;text-align: right;">Amharic Ace</b>
-          </div>
+          </div><wbr>
           <div style='top:0px;position:fixed;width:${innerWidth - 10}px;margin-right:5px;margin-top:2px;height:28px;text-align:right;font-size:28px;'>
             ${after||''}
           </div>
           <br>
-          <div style='height:28px'></div>
+          <div style='height:14px;border-style:none none solid none;border-width:2px;position:relative;'></div>
+          <div style='height:28px;'></div>
         `;
     }
 }
@@ -37,25 +38,5 @@ class Link extends HTMLElement{
     }
   }
 }
-class Image extends HTMLElement{
-  constructor(){
-    super();
-  }
-  static getObservedAttributes(){
-    return ['href', 'src'];
-  }
-  connectedCallback(){
-    this.appendChild(document.createElement('img'));
-    this.onclick = () => {alert('click')};
-  }
-  attributeChangedCallback(name, _, newValue){
-    if(name == 'href'){
-      this.onclick = () => {open(newValue)};
-    } else if(name == 'src'){
-      this.childNodes[0].src = newValue;
-    }
-  }
-}
 customElements.define('page-title', PageTitle);
 customElements.define('custom-link', Link);
-customElements.define('img-link', Image);

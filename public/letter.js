cc5d01e3ff7e36ae0dc8f5814054e34d9e4180fb
Ethanx86 <79873628+Ethanx86@users.noreply.github.com>
2023-11-10 10:04:41 -0500

    11-10-23 10:04 AM
    Updated the letter.js, and now it works.

--- public/letter.js
+++ public/letter.js
@@ -1,10 +1,19 @@
+'use-strict';
 const canvas = document.querySelector('canvas[class="drawing"]');
 const ctx = canvas.getContext('2d');
-const size = 10;
+const size = 7;
 const color = 'black';
-canvas.addEventListener('click', (e) => {
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, size / 2, 0, 2 * Math.PI);
-    ctx.fill();
+var dragging = false;
+canvas.addEventListener('mousedown',_=>dragging=true);
+canvas.addEventListener('mouseup',_=>dragging=false);
+canvas.addEventListener('mousemove', (e) => {
+    if(dragging){
+        const rect = canvas.getBoundingClientRect();
+        ctx.fillStyle = color;
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        ctx.beginPath();
+        ctx.arc(x, y, size / 2, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 });

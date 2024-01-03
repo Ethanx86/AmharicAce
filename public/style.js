ce198befd24ccbea7ead4de92069ec26e685f506
Ethanx86 <79873628+Ethanx86@users.noreply.github.com>
2024-01-03 10:06:11 -0500

    1-3-24 10:05 AM
    Working on responsive menu.

--- public/style.js
+++ public/style.js
@@ -1,6 +1,29 @@
-// Get all .middle elements
-const middleElements = document.querySelectorAll('.middle');
-// Loop through .middle elements
-middleElements.forEach((middle) => {
-    middle.style.marginLeft = `${innerWidth / 2 - middle.offsetWidth / 2}px`
-});
+// Define stylize().
+function stylize() {
+    // Get all .middle elements
+    const middleElements = document.querySelectorAll('.middle');
+    // Loop through .middle elements
+    middleElements.forEach((middle) => {
+        middle.style.marginLeft = `${innerWidth / 2 - middle.offsetWidth / 2}px`
+    });
+
+    // Do a similar thing for .topnav if the menu is overflowed.
+    // Define overflow function
+    function isOverflowing({scrollWidth, clientWidth, scrollHeight, clientHeight}){
+        return scrollWidth > clientWidth || scrollHeight > clientHeight;
+    }
+    const header = document.querySelector('.page-header');
+    if(isOverflowing(header)){
+        // Make .change elements visible.
+        // Get .change elements.
+        const changeElements = document.querySelectorAll('.change');
+        // Loop through .change elements.
+        changeElements.forEach((changeElement) => {
+            changeElement.style.display = 'visible';
+        });
+    }
+}
+// Call stylize().
+stylize();
+// Call stylize() on screenresize.
+addEventListener('resize', stylize);
